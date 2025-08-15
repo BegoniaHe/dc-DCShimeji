6cdf260d82988f3b836d222a1b1eeaa032d43846
Begonia, HE <163421589+BegoniaHe@users.noreply.github.com>
2025-08-16 04:44:35 +0800

    Update run_shimeji.bat

--- shell_script/run_shimeji.bat
+++ shell_script/run_shimeji.bat
@@ -2,7 +2,7 @@
 cd /d "%~dp0"
 echo Starting Shimeji-ee...
 
-if not exist "jre\bin\java.exe" (
+if not exist "jre\bin\javaw.exe" (
     echo Error: JRE not found! Please make sure the jre folder exists.
     pause
     exit /b 1
@@ -15,7 +15,7 @@ if not exist "target\Shimeji-ee.jar" (
 )
 
 echo Launching Shimeji-ee in the background...
-start "" "jre\bin\java.exe" -cp "target\Shimeji-ee.jar;lib\*" com.group_finity.mascot.Main
+start "" "jre\bin\javaw.exe" -cp "target\Shimeji-ee.jar;lib\*" com.group_finity.mascot.Main
 
 echo Shimeji-ee has been started successfully!
 echo You can now close this window safely.
